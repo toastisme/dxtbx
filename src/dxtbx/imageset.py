382f90f15ebfea205b136bc9b22be49280e9e557
davidmcdonagh <davidnmcdonagh@gmail.com>
2021-06-17 13:41:53 +0100

    Added case for TOFImageSet to model/__init__.py when outputting to dict. SXD data can now be imported using TOFImageSet and TOFBeam.

--- src/dxtbx/imageset.py
+++ src/dxtbx/imageset.py
@@ -12,6 +12,7 @@ from dxtbx_imageset_ext import (
     ImageGrid,
     ImageSequence,
     ImageSet,
+    ImageSetBase,
     ImageSetData,
     TOFImageSet,
 )
@@ -90,6 +91,7 @@ class MemReader:
         return ""
 
 
+<<<<<<< HEAD:src/dxtbx/imageset.py
 @boost_adaptbx.boost.python.inject_into(ImageSet)
 class _:
     """
