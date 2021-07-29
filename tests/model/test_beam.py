c23edcc2b690eb7c282c28b9e687fcec7a597878
davidmcdonagh <davidnmcdonagh@gmail.com>
2021-07-29 14:14:59 +0100

    Changed monochromatic_from_dict calls to from_dict.

--- tests/model/test_beam.py
+++ tests/model/test_beam.py
@@ -175,4 +175,4 @@ def test_beam_object_comparison():
 
 def test_beam_self_serialization():
     beam = MonochromaticBeam()
-    assert beam == BeamFactory.monochromatic_from_dict(beam.to_dict())
+    assert beam == BeamFactory.from_dict(beam.to_dict())
